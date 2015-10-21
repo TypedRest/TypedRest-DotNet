5ad830fb7b2021077f15e67896cc9e65fefbf74a
Bastian Eicher <bastian@eicher.net>
2015-10-21 16:23:40 +0200

    Started development on v0.5

--- AssemblyInfo.Global.cs
+++ AssemblyInfo.Global.cs
@@ -7,5 +7,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © 2015 1und1 Internet SE")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("0.4.0.0")]
+[assembly: AssemblyVersion("0.5")]
 [assembly: ComVisible(false)]
