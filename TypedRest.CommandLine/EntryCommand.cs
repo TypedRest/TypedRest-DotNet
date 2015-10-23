b257d5b7536da87a718fd7af1d0006435aa2db9d
Bastian Eicher <bastian@eicher.net>
2015-10-23 15:09:43 +0200

    CommandLine: Introduced EntryCommand and Executor

---
+++ TypedRest.CommandLine/EntryCommand.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TypedRest.CommandLine
+{
+    /// <summary>
+    /// Command providing an entry point to a hierachy of <see cref="IEndpoint"/>s.
+    /// </summary>
+    /// <typeparamref name="TEndpoint">The specific type of <see cref="IEndpoint"/> the command starts with.</typeparamref>
+    public class EntryCommand<TEndpoint> : CommandBase<TEndpoint>, IEnumerable<KeyValuePair<string, Func<TEndpoint, ICommand>>>
+        where TEndpoint : IEndpoint
+    {
+        /// <summary>
+        /// Creates a new REST entry command.
+        /// </summary>
+        /// <param name="endpoint">The REST endpoint this command operates on.</param>
+        public EntryCommand(TEndpoint endpoint) : base(endpoint)
+        {
+        }
+
+        private readonly Dictionary<string, Func<TEndpoint, ICommand>> _commandProviders =
+            new Dictionary<string, Func<TEndpoint, ICommand>>(StringComparer.InvariantCultureIgnoreCase);
+
+        #region Enumerable
+        public IEnumerator<KeyValuePair<string, Func<TEndpoint, ICommand>>> GetEnumerator()
+        {
+            return _commandProviders.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _commandProviders.GetEnumerator();
+        }
+        #endregion
+
+        public void Add(string name, Func<TEndpoint, ICommand> commandProvider)
+        {
+            _commandProviders.Add(name, commandProvider);
+        }
+
+        protected override ICommand GetSubCommand(string name)
+        {
+            Func<TEndpoint, ICommand> commandProvider;
+            return _commandProviders.TryGetValue(name, out commandProvider) ? commandProvider(Endpoint) : null;
+        }
+
+        protected override async Task ExecuteInnerAsync(IReadOnlyList<string> args,
+            CancellationToken cancellationToken = new CancellationToken())
+        {
+            Console.Error.WriteLine("Known commands:");
+            foreach (string name in _commandProviders.Keys)
+                Console.Error.WriteLine(name);
+
+            await base.ExecuteInnerAsync(args, cancellationToken);
+        }
+    }
+}
---
+++ TypedRest.CommandLine/Executor.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace TypedRest.CommandLine
+{
+    /// <summary>
+    /// Entry point for <see cref="ICommand"/> command-line execution.
+    /// </summary>
+    public static class Executor
+    {
+        /// <summary>
+        /// Executes commands based in command-line arguments. Performs error handling and reporting.
+        /// </summary>
+        /// <param name="entryCommand">The top-level command used to locate sub-commands.</param>
+        /// <param name="args">The command-line arguments to parse.</param>
+        /// <param name="cancellationToken">Used to cancel the request.</param>
+        /// <returns>The exit code.</returns>
+        public static async Task<int> RunAsync(ICommand entryCommand, IReadOnlyList<string> args,
+            CancellationToken cancellationToken)
+        {
+            try
+            {
+                await entryCommand.ExecuteAsync(args, cancellationToken);
+                return 0;
+            }
+                #region Error handling
+            catch (OperationCanceledException)
+            {
+                return 99;
+            }
+            catch (IndexOutOfRangeException)
+            {
+                PrintError("Missing arguments");
+                return 1;
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                PrintError("Missing arguments");
+                return 1;
+            }
+            catch (ArgumentException ex)
+            {
+                PrintError(ex.Message);
+                return 1;
+            }
+            catch (FormatException ex)
+            {
+                PrintError(ex.Message);
+                return 1;
+            }
+            catch (InvalidDataException ex)
+            {
+                PrintError(ex.Message);
+                return 2;
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                PrintError(ex.Message);
+                return 3;
+            }
+            catch (KeyNotFoundException ex)
+            {
+                PrintError(ex.Message);
+                return 4;
+            }
+            catch (InvalidOperationException ex)
+            {
+                PrintError(ex.Message);
+                return 5;
+            }
+            catch (HttpRequestException ex)
+            {
+                PrintError(ex.Message);
+                return 6;
+            }
+            catch (JsonSerializationException ex)
+            {
+                PrintError(ex.Message);
+                return 7;
+            }
+            #endregion
+        }
+
+        private static void PrintError(string message)
+        {
+            var color = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Error.WriteLine(message);
+            Console.ForegroundColor = color;
+        }
+
+        /// <summary>
+        /// Executes commands based in command-line arguments. Performs error handling and reporting and cancels on Ctrl+C.
+        /// </summary>
+        /// <param name="entryCommand">The top-level command used to locate sub-commands.</param>
+        /// <param name="args">The command-line arguments to parse.</param>
+        /// <returns>The exit code.</returns>
+        public static Task<int> RunAsync(ICommand entryCommand, IReadOnlyList<string> args)
+        {
+            return RunAsync(entryCommand, args, GetCancellationToken());
+        }
+
+        private static CancellationToken GetCancellationToken()
+        {
+            var cancellationTokenSource = new CancellationTokenSource();
+            Console.CancelKeyPress += (sender, e) =>
+            {
+                cancellationTokenSource.Cancel();
+
+                // Allow the application to finish cleanup rather than terminating immediately
+                e.Cancel = true;
+            };
+            return cancellationTokenSource.Token;
+        }
+    }
+}
