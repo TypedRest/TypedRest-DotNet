b50c82d188b8efe8c23877eb26826a3b07da0cbc
Bastian Eicher <bastian@eicher.net>
2015-11-09 14:41:50 +0100

    Made StreamViewModelBase derive from CollectionViewModelBase instead of PagedCollectionViewModelBase

--- TypedRest.Wpf/ViewModels/StreamViewModelBase.cs
+++ TypedRest.Wpf/ViewModels/StreamViewModelBase.cs
@@ -6,7 +6,7 @@
     /// <typeparam name="TEntity">The type of entity the <typeparamref name="TEndpoint"/> represents.</typeparam>
     /// <typeparam name="TEndpoint">The specific type of <see cref="IStreamEndpoint{TEntity,TElementEndpoint}"/> to operate on.</typeparam>
     /// <typeparam name="TElementEndpoint">The specific type of <see cref="IElementEndpoint{TEntity}"/> the <typeparamref name="TEndpoint"/> provides for individual <typeparamref name="TEntity"/>s.</typeparam>
-    public abstract class StreamViewModelBase<TEntity, TEndpoint, TElementEndpoint> : PagedCollectionViewModelBase<TEntity, TEndpoint, TElementEndpoint>
+    public abstract class StreamViewModelBase<TEntity, TEndpoint, TElementEndpoint> : CollectionViewModelBase<TEntity, TEndpoint, TElementEndpoint>
         where TEndpoint : IStreamEndpoint<TEntity, TElementEndpoint>
         where TElementEndpoint : class, IElementEndpoint<TEntity>
     {
@@ -16,6 +16,9 @@
         /// <param name="endpoint">The REST endpoint this view model operates on.</param>
         protected StreamViewModelBase(TEndpoint endpoint) : base(endpoint)
         {
+            CanCreate = false;
+            CanDelete = false;
+            CanUpdate = false;
         }
     }
 }
