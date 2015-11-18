70f5d4de4a3eb789d2546643989338fa387baec2
Bastian Eicher <bastian@eicher.net>
2015-11-18 16:51:39 +0100

    TypedRest.Wpf: Basic error handling

--- TypedRest.Wpf/Bootstrapper.cs
+++ TypedRest.Wpf/Bootstrapper.cs
@@ -1,7 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Reflection;
 using System.Windows;
+using System.Windows.Threading;
 using Caliburn.Micro;
 
 namespace TypedRest.Wpf
@@ -21,6 +25,25 @@ namespace TypedRest.Wpf
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             DisplayRootViewFor<T>();
+        }
+
+        protected override void OnUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            if (e.Exception is InvalidDataException
+                || e.Exception is UnauthorizedAccessException
+                || e.Exception is KeyNotFoundException
+                || e.Exception is InvalidOperationException
+                || e.Exception is IndexOutOfRangeException
+                || e.Exception is HttpRequestException)
+            {
+                MessageBox.Show(e.Exception.Message, "REST error",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+                e.Handled = true;
+            }
+            else if (e.Exception is OperationCanceledException)
+                e.Handled = true;
+
+            base.OnUnhandledException(sender, e);
         }
     }
 }
