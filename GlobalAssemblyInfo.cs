adc63760ce77b4a8bfe24a77de14f9f2ab2a22d3
Bastian Eicher <bastian@eicher.net>
2016-03-18 17:10:31 +0100

    Started development on v0.16.0.0

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -7,5 +7,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("TypedRest")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("0.15.0.0")]
+[assembly: AssemblyVersion("0.16.0.0")]
 [assembly: ComVisible(false)]
