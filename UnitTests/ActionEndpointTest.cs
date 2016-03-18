a3083fed37e20ec0c1d0ff099f944a2eed4b5a39
Andreas Bieber <andreas.bieber@axoom.com>
2016-03-18 15:25:19 +0100

    removed old unittests

--- UnitTests/ActionEndpointTest.cs
+++ UnitTests/ActionEndpointTest.cs
@@ -1,7 +1,6 @@
 ﻿using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
-using System.Web.Http;
 using FluentAssertions;
 using NUnit.Framework;
 using RichardSzalay.MockHttp;
@@ -38,52 +37,6 @@ namespace TypedRest
                 .Respond(HttpStatusCode.NoContent);
 
             await _endpoint.TriggerAsync();
-        }
-
-        [Test]
-        public async Task TestTriggerWithBody()
-        {
-            Mock.Expect(HttpMethod.Post, "http://localhost/endpoint")
-                .Respond(HttpStatusCode.Accepted);
-
-            await _endpoint.TriggerAsync(new MockEntity(4711, "mock"));
-        }
-
-        [Test]
-        public async Task TestTriggerWithBodyAndResult()
-        {
-            Mock.Expect(HttpMethod.Post, "http://localhost/endpoint")
-                .Respond(TriggerHandler);
-
-            var entity = new MockEntity(4711, "mock");
-            long id = await _endpoint.TriggerAsync<MockEntity,long>(entity);
-
-            id.Should().Be(entity.Id);
-        }
-
-        [Test]
-        public async Task TestTriggerWithResult()
-        {
-            Mock.Expect(HttpMethod.Post, "http://localhost/endpoint")
-                .Respond(TriggerHandler);
-
-            long id = await _endpoint.TriggerAsync<long>();
-
-            id.Should().Be(1234);
-        }
-
-        private static async Task<HttpResponseMessage> TriggerHandler(HttpRequestMessage httpRequestMessage)
-        {
-            long result = 1234;
-
-            if (httpRequestMessage.Content != null)
-            {
-                var content = await httpRequestMessage.Content.ReadAsAsync<MockEntity>();
-                result = content.Id;
-            }
-
-            httpRequestMessage.SetConfiguration(new HttpConfiguration());
-            return httpRequestMessage.CreateResponse(HttpStatusCode.Accepted, result);
         }
     }
 }
