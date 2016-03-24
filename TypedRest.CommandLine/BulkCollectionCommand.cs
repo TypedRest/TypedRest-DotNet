2f06fb8f46444553a04d041bb45ebe461d526fe4
Bastian Eicher <bastian@eicher.net>
2016-03-24 10:25:39 +0100

    BulkCollectionCommand: Replaced class dependency with interface dependency

--- TypedRest.CommandLine/BulkCollectionCommand.cs
+++ TypedRest.CommandLine/BulkCollectionCommand.cs
@@ -4,13 +4,13 @@
     /// Command operating on a <see cref="BulkCollectionEndpoint{TEntity}"/>.
     /// </summary>
     /// <typeparam name="TEntity">The type of entity the <see cref="BulkCollectionEndpoint{TEntity}"/> represents.</typeparam>
-    public class BulkCollectionCommand<TEntity> : BulkCollectionCommandBase<TEntity, BulkCollectionEndpoint<TEntity>, IElementEndpoint<TEntity>>
+    public class BulkCollectionCommand<TEntity> : BulkCollectionCommandBase<TEntity, IBulkCollectionEndpoint<TEntity>, IElementEndpoint<TEntity>>
     {
         /// <summary>
         /// Creates a new REST Bulk collection command.
         /// </summary>
         /// <param name="endpoint">The REST endpoint this command operates on.</param>
-        public BulkCollectionCommand(BulkCollectionEndpoint<TEntity> endpoint) : base(endpoint)
+        public BulkCollectionCommand(IBulkCollectionEndpoint<TEntity> endpoint) : base(endpoint)
         {
         }
 
