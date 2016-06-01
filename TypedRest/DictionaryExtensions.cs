7c61cc9157bd14fb0f697d86bdb8d0d478a1c4f0
Bastian Eicher <bastian@eicher.net>
2016-06-01 14:22:08 +0200

    Improved DictionaryExtensions

--- TypedRest/DictionaryExtensions.cs
+++ TypedRest/DictionaryExtensions.cs
@@ -1,22 +1,46 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace TypedRest
 {
     /// <summary>
     /// Provides extension methods for <seealso cref="IDictionary{TKey,TValue}"/>s.
     /// </summary>
-    public static class DictionaryExtensions
+    internal static class DictionaryExtensions
     {
         /// <summary>
         /// Returns the element with the specified <paramref name="key"/> from the <paramref name="dictionary"/>.
-        /// Creates, adds and returns a new <typeparamref name="TValue"/> if no match was found.
+        /// Creates, adds and returns a new value using <paramref name="valueFactory"/> if no match was found.
+        /// </summary>
+        public static TValue GetOrAdd<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key,
+            Func<TValue> valueFactory)
+        {
+            TValue value;
+            if (!dictionary.TryGetValue(key, out value))
+                dictionary.Add(key, value = valueFactory());
+            return value;
+        }
+
+        /// <summary>
+        /// Returns the element with the specified <paramref name="key"/> from the <paramref name="dictionary"/>.
+        /// Creates, adds and returns a new <typeparamref name="TValue"/> instance if no match was found.
         /// </summary>
         public static TValue GetOrAdd<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key)
             where TValue : new()
         {
+            return dictionary.GetOrAdd(key, () => new TValue());
+        }
+
+        /// <summary>
+        /// Returns the element with the specified <paramref name="key"/> from the <paramref name="dictionary"/>.
+        /// Creates, adds and returns a new value using <paramref name="valueFactory"/> if no match was found.
+        /// </summary>
+        public static async Task<TValue> GetOrAddAsync<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, Func<Task<TValue>> valueFactory)
+        {
             TValue value;
             if (!dictionary.TryGetValue(key, out value))
-                dictionary.Add(key, value = new TValue());
+                dictionary.Add(key, value = await valueFactory());
             return value;
         }
     }
