7f01026ac29b8df40d1973b351339ef60b4e0cc0
Bastian Eicher <bastian@eicher.net>
2016-07-08 13:24:17 +0200

    Minor tweak to template

--- Templates/WebAPI/WebService/Controllers/EntryController.cs
+++ Templates/WebAPI/WebService/Controllers/EntryController.cs
@@ -1,8 +1,13 @@
 ﻿using System.Web.Http;
+using System.Web.Http.Description;
 using WebApi.LinkHeader;
 
 namespace XProjectNamespaceX.WebService.Controllers
 {
+    /// <summary>
+    /// Entry point for XProjectNameX API.
+    /// </summary>
+    [ApiExplorerSettings(IgnoreApi = true)]
     public class EntryController : ApiController
     {
         /// <summary>
