444b14201824a1ce0a3cad66d6c408165d99fb88
Bastian Eicher <bastian@eicher.net>
2016-08-01 17:19:55 +0200

    WebAPI template: Retargeted WebService to .NET 4.5.1, updated NuGet dependencies

--- Templates/WebAPI/Client/MyEntryEndpoint.cs
+++ Templates/WebAPI/Client/MyEntryEndpoint.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Configuration;
 using System.Net;
+using System.Net.Http.Headers;
 using XProjectNamespaceX.Model;
 using TypedRest;
 
@@ -12,7 +13,7 @@ namespace XProjectNamespaceX.Client
     public class MyEntryEndpoint : EntryEndpoint
     {
         /// <summary>
-        /// Configures a connection to the XProjectNameX API.
+        /// Creates a new XProjectNameX entry point.
         /// </summary>
         /// <param name="uri">The base URI of the REST interface. Missing trailing slash will be appended automatically.</param>
         /// <param name="credentials">The credentials used to authenticate against the REST interface. Extracts credentials from <paramref name="uri"/> is unset.</param>
@@ -22,10 +23,29 @@ namespace XProjectNamespaceX.Client
         }
 
         /// <summary>
-        /// Configures a connection to the XProjectNameX API using credentials from ConnectionStrings.
+        /// Creates a new XProjectNameX entry point using URI (with credentials) from ConnectionStrings.
         /// </summary>
         public MyEntryEndpoint()
             : this(new Uri(ConfigurationManager.ConnectionStrings["XProjectNamespaceX"].ConnectionString))
+        {
+        }
+
+        /// <summary>
+        /// Creates a new XProjectNameX entry point using a token for authentication.
+        /// </summary>
+        /// <param name="uri">The base URI of the REST interface. Missing trailing slash will be appended automatically.</param>
+        /// <param name="token">The token used to authenticate against the REST interface.</param>
+        public MyEntryEndpoint(Uri uri, string token)
+            : base(uri)
+        {
+            HttpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        }
+
+        /// <summary>
+        /// Creates a new XProjectNameX entry point using URI from ConnectionStrings and a token for authentication.
+        /// </summary>
+        public MyEntryEndpoint(string token)
+            : this(new Uri(ConfigurationManager.ConnectionStrings["XProjectNamespaceX"].ConnectionString), token)
         {
         }
 
