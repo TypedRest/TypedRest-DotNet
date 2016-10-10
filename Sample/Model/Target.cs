a5fe333a2a4121b98234ff80601266c2a4fc6cdd
Bastian Eicher <bastian@eicher.net>
2016-10-10 14:39:09 +0200

    Removed Sample projects (superseded by Templates)

--- Sample/Model/Target.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TypedRestSample.Model
-{
-    /// <summary>
-    /// A specific target to deploy <see cref="Resource"/>s to.
-    /// </summary>
-    public class Target : NamedEntity
-    {
-    }
-}
