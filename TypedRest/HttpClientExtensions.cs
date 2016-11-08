459be35437d2ca10054829e48a8b884b1aa2c22a
Bastian Eicher <bastian@eicher.net>
2016-11-08 10:06:54 +0100

    Added HttpClientExtensions.PatchAsync()

--- TypedRest/HttpClientExtensions.cs
+++ TypedRest/HttpClientExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net.Http;
+using System.Net.Http.Formatting;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -13,17 +14,19 @@ namespace TypedRest
         /// <summary>
         /// Send a HEAD request to the specified URI.
         /// </summary>
-        public static Task<HttpResponseMessage> HeadAsync(this HttpClient httpClient, Uri uri, CancellationToken cancellationToken = default(CancellationToken))
-        {
-            return httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Head, uri), cancellationToken);
-        }
+        public static Task<HttpResponseMessage> HeadAsync(this HttpClient httpClient, Uri uri, CancellationToken cancellationToken = default(CancellationToken)) =>
+            httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Head, uri), cancellationToken);
 
         /// <summary>
         /// Send an OPTIONS request to the specified URI.
         /// </summary>
-        public static Task<HttpResponseMessage> OptionsAsync(this HttpClient httpClient, Uri uri, CancellationToken cancellationToken = default(CancellationToken))
-        {
-            return httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Options, uri), cancellationToken);
-        }
+        public static Task<HttpResponseMessage> OptionsAsync(this HttpClient httpClient, Uri uri, CancellationToken cancellationToken = default(CancellationToken)) =>
+            httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Options, uri), cancellationToken);
+
+        public static Task<HttpResponseMessage> PatchAsync<T>(this HttpClient httpClient, Uri uri, T value, MediaTypeFormatter formatter, CancellationToken cancellationToken = default(CancellationToken)) =>
+            httpClient.SendAsync(new HttpRequestMessage(new HttpMethod("PATCH"), uri)
+            {
+                Content = new ObjectContent<T>(value, formatter)
+            }, cancellationToken);
     }
 }
