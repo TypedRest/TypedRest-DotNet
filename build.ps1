25ec65057d6e919f745caa4ce99d112aa599c6a6
Bastian Eicher <bastian@eicher.net>
2017-06-06 13:05:54 +0200

    Fixed build script

--- build.ps1
+++ build.ps1
@@ -3,9 +3,7 @@ $ErrorActionPreference = "Stop"
 
 pushd "$(Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)"
 
-dotnet clean
-dotnet msbuild /t:Restore /p:Configuration=Release /p:Version=$Version
-dotnet msbuild /t:Build /p:Configuration=Release /p:Version=$Version
+dotnet msbuild /t:Restore /t:Build /p:Configuration=Release /p:Version=$Version
 dotnet test --configuration Release --no-build "UnitTests/UnitTests.csproj"
 
 popd
