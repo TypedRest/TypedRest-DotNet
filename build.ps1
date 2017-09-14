3d0ebb129cd996db81880fb3a52b4087c9008b33
Bastian Eicher <bastian@eicher.net>
2017-09-14 14:22:01 +0200

    Minor tweaks

--- build.ps1
+++ build.ps1
@@ -1,8 +1,8 @@
 Param ([Parameter(Mandatory=$True)][string]$Version)
 $ErrorActionPreference = "Stop"
+pushd $(Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
 
-pushd "$(Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)"
-
+dotnet clean
 dotnet msbuild /t:Restore /t:Build /p:Configuration=Release /p:Version=$Version
 dotnet test --configuration Release --no-build "UnitTests/UnitTests.csproj"
 
