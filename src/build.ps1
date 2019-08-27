b4e5c1c055301f79f535bd9ec998d2ab613386c7
Bastian Eicher <bastian@eicher.net>
2019-08-27 20:24:54 +0200

    Improved error handling PowerShell scripts

--- src/build.ps1
+++ src/build.ps1
@@ -3,5 +3,6 @@ $ErrorActionPreference = "Stop"
 pushd $PSScriptRoot
 
 dotnet msbuild -t:Restore -t:Build -p:Configuration=Release -p:Version=$Version
+if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
 
 popd
--- src/test.ps1
+++ src/test.ps1
@@ -2,5 +2,6 @@ $ErrorActionPreference = "Stop"
 pushd $PSScriptRoot
 
 dotnet test --configuration Release --no-build UnitTests\UnitTests.csproj
+if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
 
 popd
