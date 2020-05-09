2027a4a49e264df07343dda34495890f56045d7a
Bastian Eicher <bastian@eicher.net>
2020-05-09 22:34:00 +0200

    Removed separate build target for Linux

--- src/build.sh
+++ src/build.sh
@@ -4,4 +4,4 @@ cd `dirname $0`
 
 echo "WARNING: You need Visual Studio on Windows to perform a full Release build"
 
-dotnet msbuild -v:Quiet -t:Restore -t:Build -p:Configuration=DebugLinux -p:Version=${1:-1.0-dev}
+dotnet msbuild -v:Quiet -t:Restore -t:Build -p:Configuration=Debug -p:Version=${1:-1.0-dev}
