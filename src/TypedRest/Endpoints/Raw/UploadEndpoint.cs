9bc98d2964b569671153e705a824a64dc687d283
Bastian Eicher <bastian@eicher.net>
2020-08-20 19:51:53 +0200

    Simplified UploadEndpoint constructors using optional parameters

--- src/TypedRest/Endpoints/Raw/UploadEndpoint.cs
+++ src/TypedRest/Endpoints/Raw/UploadEndpoint.cs
@@ -8,50 +8,32 @@ using System.Threading.Tasks;
 namespace TypedRest.Endpoints.Raw
 {
     /// <summary>
-    /// Endpoint that accepts binary uploads.
+    /// Endpoint that accepts binary uploads using multi-part form encoding or raw bodies.
     /// </summary>
     public class UploadEndpoint : EndpointBase, IUploadEndpoint
     {
         private readonly string? _formField;
 
         /// <summary>
-        /// Creates a new upload endpoint using raw bodies.
+        /// Creates a new upload endpoint.
         /// </summary>
         /// <param name="referrer">The endpoint used to navigate to this one.</param>
         /// <param name="relativeUri">The URI of this endpoint relative to the <paramref name="referrer"/>'s.</param>
-        public UploadEndpoint(IEndpoint referrer, Uri relativeUri)
+        /// <param name="formField">The name of the form field to place the uploaded data into; leave <c>null</c> to use raw bodies instead of a multi-part forms.</param>
+        public UploadEndpoint(IEndpoint referrer, Uri relativeUri, string? formField = null)
             : base(referrer, relativeUri)
-        {}
-
-        /// <summary>
-        /// Creates a new upload endpoint using multi-part form encoding.
-        /// </summary>
-        /// <param name="referrer">The endpoint used to navigate to this one.</param>
-        /// <param name="relativeUri">The URI of this endpoint relative to the <paramref name="referrer"/>'s.</param>
-        /// <param name="formField">The name of the form field to place the uploaded data into.</param>
-        public UploadEndpoint(IEndpoint referrer, Uri relativeUri, string formField)
-            : this(referrer, relativeUri)
         {
             _formField = formField;
         }
 
         /// <summary>
-        /// Creates a new upload endpoint using raw bodies.
-        /// </summary>
-        /// <param name="referrer">The endpoint used to navigate to this one.</param>
-        /// <param name="relativeUri">The URI of this endpoint relative to the <paramref name="referrer"/>'s. Prefix <c>./</c> to append a trailing slash to the <paramref name="referrer"/> URI if missing.</param>
-        public UploadEndpoint(IEndpoint referrer, string relativeUri)
-            : base(referrer, relativeUri)
-        { }
-
-        /// <summary>
         /// Creates a new upload endpoint using multi-part form encoding.
         /// </summary>
         /// <param name="referrer">The endpoint used to navigate to this one.</param>
         /// <param name="relativeUri">The URI of this endpoint relative to the <paramref name="referrer"/>'s.</param>
-        /// <param name="formField">The name of the form field to place the uploaded data into.</param>
-        public UploadEndpoint(IEndpoint referrer, string relativeUri, string formField)
-            : this(referrer, relativeUri)
+        /// <param name="formField">The name of the form field to place the uploaded data into; leave <c>null</c> to use raw bodies instead of a multi-part forms.</param>
+        public UploadEndpoint(IEndpoint referrer, string relativeUri, string? formField = null)
+            : base(referrer, relativeUri)
         {
             _formField = formField;
         }
