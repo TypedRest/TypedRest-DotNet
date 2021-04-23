eccb21e95d2a558b9e33aea99afed18fe88aac7a
Bastian Eicher <bastian@eicher.net>
2021-04-23 10:40:43 +0200

    Marked .NoContext() extension method for aggressive inlining

--- src/TypedRest/TaskExtensions.cs
+++ src/TypedRest/TaskExtensions.cs
@@ -11,12 +11,14 @@ namespace TypedRest
         /// <summary>
         /// Convenience wrapper for <see cref="Task.ConfigureAwait"/><c>(false)</c>.
         /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static ConfiguredTaskAwaitable NoContext(this Task task)
             => task.ConfigureAwait(continueOnCapturedContext: false);
 
         /// <summary>
         /// Convenience wrapper for <see cref="Task{T}.ConfigureAwait"/><c>(false)</c>.
         /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static ConfiguredTaskAwaitable<TResult> NoContext<TResult>(this Task<TResult> task)
             => task.ConfigureAwait(continueOnCapturedContext: false);
     }
